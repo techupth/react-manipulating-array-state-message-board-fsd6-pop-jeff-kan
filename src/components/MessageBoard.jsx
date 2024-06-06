bde3c3f8ae6481d6b91ab27949433bfb65660123
Pop <shounen.pop@gmail.com>
2024-06-06 13:33:01 +0700

    feat: event add and remove array state

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,50 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [message, setMessage] = useState("");
+  const [showMessage, setShowMessage] = useState([]);
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button
+          className="submit-message-button"
+          onClick={() => {
+            message !== ""
+              ? setShowMessage([...showMessage, message])
+              : alert("กรอกก่อนเด้อสู");
+            setMessage("");
+          }}
+        >
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {showMessage.map((msn, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{msn}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  setShowMessage(showMessage.toSpliced(index, 1));
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
